0b62c842b544766e14ed79b5e57e0b30d9c4e635
Eduardo Gonzalez <eg9800@gmail.com>
2019-08-11 22:58:59 -0700

    added styling to html file

--- major.js
+++ major.js
@@ -2,23 +2,29 @@
   function startTheSearch (){
     $('.start').on('click','.startbutton', function(event){
       //alert('start button pressed');
-      // $('.navHide').css('display','block');
+      $('.hide').css('display','block');
       $('.info').hide();
       $('.apinfo').hide();
       $('.start').hide();
+      
       $(searchCountry);
     })
 
   };
 
   function searchCountry (){
-    $('.searchBlock')
+    $('.searchBlock').on('click', '.search',  function(event){
+      // alert('search initiated')
+      $('.searchBlock').hide();
+      $('.rhide').css('display', 'block');
+    })
   }
 
 
 
 
+  function start (){
+    $(startTheSearch)
+  }
 
-
-
-  $(startTheSearch);
+  $(start);
