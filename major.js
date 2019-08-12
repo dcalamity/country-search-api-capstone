fe99cb16952bffd14abe6a6ef28fba8f44f052f1
Eduardo Gonzalez <eg9800@gmail.com>
2019-08-11 21:54:09 -0700

    made changes to hide certain tags, and created the start search function

---
+++ major.js
@@ -0,0 +1,24 @@
+
+  function startTheSearch (){
+    $('.start').on('click','.startbutton', function(event){
+      //alert('start button pressed');
+      // $('.navHide').css('display','block');
+      $('.info').hide();
+      $('.apinfo').hide();
+      $('.start').hide();
+      $(searchCountry);
+    })
+
+  };
+
+  function searchCountry (){
+    $('.searchBlock')
+  }
+
+
+
+
+
+
+
+  $(startTheSearch);
