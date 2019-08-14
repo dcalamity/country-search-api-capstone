831c1a5bdadd1539b77fdf0a367684dd807c3b3e
Eduardo Gonzalez <eg9800@gmail.com>
2019-08-13 23:34:29 -0700

    added functions to display data from the weather API, added a function to restart the application as well.

--- major.js
+++ major.js
@@ -4,6 +4,8 @@
 const weatherkey = '01dd76c234fd91af1d022bb5b85eb549';
 const weatherSearchUrl = "https://api.openweathermap.org/data/2.5/weather";
 
+const fUnit = "imperial";
+
   function startTheSearch (){
     $('.start').on('click','.startbutton', function(event){
       //alert('start button pressed');
@@ -26,22 +28,23 @@ const weatherSearchUrl = "https://api.openweathermap.org/data/2.5/weather";
     $('.searchBlock').on('click', '.search',  function(event){
       // alert('search initiated')
       $('.searchBlock').hide();
-      $('.rhide').css('display', 'block');
       const countryId = $("#countryls option:selected").val();
       const countryName = $("#countryls option:selected").text();
       console.log(countryId);
       console.log(countryName);
-      getTheWeather(countryId)
+      getTheWeather(countryId, countryName)
+      
     })
     
   }
 
-  function getTheWeather(countryId) {
+  function getTheWeather(countryId, countryName) {
     const params = {
       id: countryId,
       APPID: weatherkey,
-
+      units: fUnit,
     }
+
     console.log(params);
     const queryString = formatQueryParams(params);
     console.log(queryString);
@@ -54,20 +57,45 @@ const weatherSearchUrl = "https://api.openweathermap.org/data/2.5/weather";
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayWeatherResults(responseJson))
+    .then(responseJson => displayWeatherResults(responseJson, countryName))
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
+
     });
+    $('.rhide').css('display', 'block');
   }
-
+  //instead received the temp in the format we wanted from API
+  // function converstion (temp){
+  //   let fahtValue = 9 / 5 * (temp - 273) + 32;
+  //   let round = Math.round(fahtValue);
+  //   console.log(round)
+  //   return round;
+  // }
   
-  function displayWeatherResults (responseJson){
+  function displayWeatherResults(responseJson, countryName){
+    $('#weatherResults').empty();
+    console.log(responseJson)
+      $('#weatherResults').append(
 
+        `<h2>The weather in ${countryName} </h2>
+        <p>How does the sky look? <br> ${responseJson.weather[0].main}</p>
+        <li>${responseJson.main.temp} &#8457</li>`
+      )
   }
+
+  //function listens for the the restart button being pressed
+function restartQuiz() {
+  $('.restartBox').on('click', '.restart', function(event){
+      event.preventDefault()
+      console.log('clicked on restart button')
+      window.location.reload();
+  }
+)}
 
 
   function start (){
     $(startTheSearch)
+    $(restartQuiz)
   }
 
   $(start);
