9d017233b5202209bb4797c8f75065cdf7e7466a
Eduardo Gonzalez <eg9800@gmail.com>
2019-08-18 22:41:47 -0700

    added function to display the youtube results structures them using flex box row and have it set up for 6 video results

--- major.js
+++ major.js
@@ -53,6 +53,7 @@
     const params = {
       q: searchTerm,
       part: 'snippet',
+      maxResults: '6',
       type: 'video',
       key: youtubeAPIKey,
     }
@@ -76,8 +77,18 @@
   }
 
     function displayYoutubeResults(videoResults){
-      
-    }
+      console.log(videoResults)
+      $('.youTubeResults').empty();
+      for (let i = 0; i < videoResults.items.length; i++){
+        $('.youTubeResults').append(
+          `<section role="video results" class="yResults" class="yTubeResultBox"><h3>${videoResults.items[i].snippet.title}</h3>
+          <p>${videoResults.items[i].snippet.description}</p>
+          <a href="https://www.youtube.com/watch?v=${videoResults.items[0].id.videoId}">
+          <img class="videoImage" src='${videoResults.items[i].snippet.thumbnails.high.url}'>
+          </a></section>`
+        
+      )}
+    };
   
 
   function searchHeadLines (searchTerm){
